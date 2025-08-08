b8774af5917c93b869be556e29bbae3ecf53035c
Rishiraj Dhar <rishirajdhr@gmail.com>
2025-08-08 09:09:00 -0400

    build: add prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 80,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
+
+export default config; 
