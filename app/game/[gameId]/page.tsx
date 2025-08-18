343ecac82dea67686c8d9f01041a64774ac1bc5b
Rishiraj Dhar <rishirajdhr@gmail.com>
2025-08-18 12:11:52 -0400

    feat(game): add play route link on landing page

--- app/game/[gameId]/page.tsx
+++ app/game/[gameId]/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { getGameById } from "@/features/games/services";
+import { Play } from "lucide-react";
+import Link from "next/link";
 
 export default async function GamePage({
   params,
@@ -14,11 +17,15 @@ export default async function GamePage({
 
   return (
     <main className="grid h-full w-full place-items-center text-center">
-      <section className="max-w-lg">
-        <h1 className="py-4 text-6xl font-semibold tracking-tighter">
-          {game.name}
-        </h1>
+      <section className="max-w-lg space-y-4">
+        <h1 className="text-6xl font-semibold tracking-tighter">{game.name}</h1>
         <p className="font-light text-gray-700">{game.description}</p>
+        <Button variant="outline" asChild>
+          <Link href={`/game/${gameId}/play`}>
+            <Play className="text-gray-800" />
+            <span className="text-gray-800">Play</span>
+          </Link>
+        </Button>
       </section>
     </main>
   );
