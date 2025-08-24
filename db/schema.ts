d501995422929c436a49e3a913b53f517a9cb6ba
Rishiraj Dhar <rishirajdhr@gmail.com>
2025-08-24 13:04:39 -0500

    feat(db)!: update schema to create behavior tables

--- db/schema.ts
+++ db/schema.ts
@@ -2,108 +2,138 @@ import { relations } from "drizzle-orm";
 import type { AnyPgColumn } from "drizzle-orm/pg-core";
 import { pgTable, bigint, timestamp, text } from "drizzle-orm/pg-core";
 
+//#region ===== TABLES =====
+
+const timestamps = {
+  createdAt: timestamp("created_at", { withTimezone: true, mode: "string" })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", {
+    withTimezone: true,
+    mode: "string",
+  }).notNull(),
+};
+
 export const games = pgTable("games", {
+  ...timestamps,
   id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
-  createdAt: timestamp("created_at", { withTimezone: true, mode: "string" })
-    .defaultNow()
-    .notNull(),
   name: text().notNull(),
   description: text().default("").notNull(),
-  startRoomId: bigint("start_room_id", { mode: "number" }).references(
-    (): AnyPgColumn => rooms.id,
-    {
-      onDelete: "set null",
-      onUpdate: "cascade",
-    }
-  ),
 });
-
-export const gamesRelations = relations(games, ({ many }) => ({
-  rooms: many(rooms),
-  entities: many(entities),
-}));
-
-export const rooms = pgTable("rooms", {
-  id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
-  createdAt: timestamp("created_at", { withTimezone: true, mode: "string" })
-    .defaultNow()
-    .notNull(),
-  name: text().notNull(),
-  description: text().default("").notNull(),
-  northExit: bigint("north_exit", { mode: "number" })
-    .unique()
-    .references((): AnyPgColumn => rooms.id, {
-      onDelete: "set null",
-      onUpdate: "cascade",
-    }),
-  southExit: bigint("south_exit", { mode: "number" })
-    .unique()
-    .references((): AnyPgColumn => rooms.id, {
-      onDelete: "set null",
-      onUpdate: "cascade",
-    }),
-  eastExit: bigint("east_exit", { mode: "number" })
-    .unique()
-    .references((): AnyPgColumn => rooms.id, {
-      onDelete: "set null",
-      onUpdate: "cascade",
-    }),
-  westExit: bigint("west_exit", { mode: "number" })
-    .unique()
-    .references((): AnyPgColumn => rooms.id, {
-      onDelete: "set null",
-      onUpdate: "cascade",
-    }),
-  gameId: bigint("game_id", { mode: "number" })
-    .notNull()
-    .references(() => games.id, {
-      onDelete: "cascade",
-      onUpdate: "cascade",
-    }),
-});
-
-export const roomsRelations = relations(rooms, ({ many, one }) => ({
-  northExit: one(rooms, {
-    fields: [rooms.northExit],
-    references: [rooms.id],
-  }),
-  southExit: one(rooms, {
-    fields: [rooms.southExit],
-    references: [rooms.id],
-  }),
-  eastExit: one(rooms, {
-    fields: [rooms.eastExit],
-    references: [rooms.id],
-  }),
-  westExit: one(rooms, {
-    fields: [rooms.westExit],
-    references: [rooms.id],
-  }),
-
-  entities: many(entities),
-  game: one(games, { fields: [rooms.gameId], references: [games.id] }),
-}));
 
 export const entities = pgTable("entities", {
+  ...timestamps,
   id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
-  createdAt: timestamp("created_at", { withTimezone: true, mode: "string" })
-    .defaultNow()
-    .notNull(),
-  name: text().notNull(),
-  description: text().default("").notNull(),
-  roomId: bigint("room_id", { mode: "number" }).references(() => rooms.id, {
-    onDelete: "set null",
-    onUpdate: "cascade",
-  }),
   gameId: bigint("game_id", { mode: "number" })
-    .notNull()
     .references(() => games.id, {
       onDelete: "cascade",
+      onUpdate: "cascade",
+    })
+    .notNull(),
+});
+
+const behaviorIds = {
+  id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
+  entityId: bigint("entity_id", { mode: "number" })
+    .references(() => entities.id, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    })
+    .notNull(),
+  gameId: bigint("game_id", { mode: "number" })
+    .references(() => games.id, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    })
+    .notNull(),
+};
+
+export const describables = pgTable("describables", {
+  ...timestamps,
+  ...behaviorIds,
+  name: text().notNull(),
+  description: text().default("").notNull(),
+});
+
+export const navigables = pgTable("navigables", {
+  ...timestamps,
+  ...behaviorIds,
+  north: bigint({ mode: "number" })
+    .unique()
+    .references((): AnyPgColumn => navigables.id, {
+      onDelete: "set null",
+      onUpdate: "cascade",
+    }),
+  south: bigint({ mode: "number" })
+    .unique()
+    .references((): AnyPgColumn => navigables.id, {
+      onDelete: "set null",
+      onUpdate: "cascade",
+    }),
+  east: bigint({ mode: "number" })
+    .unique()
+    .references((): AnyPgColumn => navigables.id, {
+      onDelete: "set null",
+      onUpdate: "cascade",
+    }),
+  west: bigint({ mode: "number" })
+    .unique()
+    .references((): AnyPgColumn => navigables.id, {
+      onDelete: "set null",
       onUpdate: "cascade",
     }),
 });
 
-export const entitiesRelations = relations(entities, ({ one }) => ({
-  room: one(rooms, { fields: [entities.roomId], references: [rooms.id] }),
-  game: one(games, { fields: [entities.gameId], references: [games.id] }),
+//#endregion
+
+//#region ===== RELATIONS =====
+
+export const gamesRelations = relations(games, ({ many }) => ({
+  entities: many(entities),
+  describables: many(describables),
+  navigables: many(navigables),
 }));
+
+export const entitiesRelations = relations(entities, ({ one, many }) => ({
+  game: one(games, { fields: [entities.gameId], references: [games.id] }),
+  describables: many(describables),
+  navigables: many(navigables),
+}));
+
+export const describablesRelations = relations(describables, ({ one }) => ({
+  game: one(games, { fields: [describables.gameId], references: [games.id] }),
+  entity: one(entities, {
+    fields: [describables.entityId],
+    references: [entities.id],
+  }),
+}));
+
+export const navigablesRelations = relations(navigables, ({ one }) => ({
+  game: one(games, { fields: [navigables.gameId], references: [games.id] }),
+  entity: one(entities, {
+    fields: [navigables.entityId],
+    references: [entities.id],
+  }),
+  northExit: one(navigables, {
+    fields: [navigables.north],
+    references: [navigables.id],
+    relationName: "north_exit",
+  }),
+  southExit: one(navigables, {
+    fields: [navigables.south],
+    references: [navigables.id],
+    relationName: "south_exit",
+  }),
+  eastExit: one(navigables, {
+    fields: [navigables.east],
+    references: [navigables.id],
+    relationName: "east_exit",
+  }),
+  westExit: one(navigables, {
+    fields: [navigables.west],
+    references: [navigables.id],
+    relationName: "west_exit",
+  }),
+}));
+
+//#endregion
