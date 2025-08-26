312b0ed4a61b9e103f5c63f9d1c687528b1934a7
Rishiraj Dhar <rishirajdhr@gmail.com>
2025-08-25 19:04:44 -0500

    feat(jest): configure coverage options
    Turn off test coverage by default and make it opt-in. Setup a script to
    run tests with coverage and configure jest to collect coverage from all
    relevant source files.

--- jest.config.ts
+++ jest.config.ts
@@ -22,18 +22,16 @@ const config: Config = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ["**/*.{js,ts,jsx,tsx}", "!**/*.test.{js,ts,jsx,tsx}"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ["/node_modules/", "/.next/"],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
