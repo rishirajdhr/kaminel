a7abb88054308ffe756f258ae05184b2b80877da
Rishiraj Dhar <rishirajdhr@gmail.com>
2025-08-26 18:01:27 -0500

    fix(room): update schemas and dependent services

--- features/games/schema.ts
+++ features/games/schema.ts
@@ -1,5 +1,9 @@
-import { describables, games, navigables } from "@/db/schema";
-import { createSelectSchema } from "drizzle-zod";
+import { describables, entities, games, navigables } from "@/db/schema";
+import {
+  createInsertSchema,
+  createSelectSchema,
+  createUpdateSchema,
+} from "drizzle-zod";
 import { z } from "zod";
 
 /** Validation schema for a game. */
@@ -8,8 +12,30 @@ export const gameSchema = createSelectSchema(games);
 /** Validation schema for a describable. */
 export const describableSchema = createSelectSchema(describables);
 
+export const newDescribableSchema = createInsertSchema(describables).omit({
+  updatedAt: true,
+});
+
+export const updatedDescribableSchema = createUpdateSchema(describables).omit({
+  updatedAt: true,
+});
+
+export const entitySchema = createSelectSchema(entities);
+
+export const newEntitySchema = createInsertSchema(entities).omit({
+  updatedAt: true,
+});
+
 /** Validation schema for a navigable. */
 export const navigableSchema = createSelectSchema(navigables);
+
+export const newNavigableSchema = createInsertSchema(navigables).omit({
+  updatedAt: true,
+});
+
+export const updatedNavigableSchema = createUpdateSchema(navigables).omit({
+  updatedAt: true,
+});
 
 /** Validation schema for a game graph. */
 export const gameGraphSchema = gameSchema.extend({
