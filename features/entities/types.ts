0e803be2666cb751c37237d847a77c8b50cd46f2
Rishiraj Dhar <rishirajdhr@gmail.com>
2025-08-26 18:04:01 -0500

    chore(entity): remove old feature files

--- features/entities/types.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { entitySchema, newEntitySchema } from "./schema";
-
-/** Represents an existing entity in the game. */
-export type Entity = z.infer<typeof entitySchema>;
-
-/** Represents the subset of data needed to create an entity. */
-export type NewEntity = z.infer<typeof newEntitySchema>;
